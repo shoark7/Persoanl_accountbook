e73892874631f95c5f2888d317274a8f13b77b7d
Sunghwan Park <shoark7@daum.net>
2016-06-22 00:52:17 +0900

    Add 1.1 function and file upload function

--- expenditure_tracker.py
+++ expenditure_tracker.py
@@ -1,4 +1,5 @@
 #expenditure_tracker.py
+#프로젝트 시작 날짜 : 2016-06-18
 """ 
 컴퓨터 시작 시 자동으로 시작되고, 
 그래서 매일의 돈 지출의 기록을 기록, 추적할 수 있도록 하는 것이 핵심이다.
@@ -6,36 +7,39 @@
 """
 
 import datetime
-import json
+# import json
 import sys
 import time
+import pickle
 
 # 비밀번호 설정. 내가 아닌 다른 사람이 입력하는 것을 방지한다.
-try:
-	input_password = sys.argv[1]
+# try:
+# 	input_password = sys.argv[1]
 
-except:
-	input_password = input("비밀번호를 입력하셔야 합니다. : ")
-	for i in range(2):
-		if input_password != "696238":
-			input_password = input("틀렸습니다. 다시 입력하세요 : ")
+# except:
+# 	input_password = input("비밀번호를 입력하셔야 합니다. : ")
+# 	for i in range(2):
+# 		if input_password != "696238":
+# 			input_password = input("틀렸습니다. 다시 입력하세요 : ")
 
-	if input_password != "696238":
-		sys.exit()
-else:
-	for i in range(2):
-		if input_password != "696238":
-			input_password = input("틀렸습니다. 다시 입력하세요 : ")
+# 	if input_password != "696238":
+# 		sys.exit()
+# else:
+# 	for i in range(2):
+# 		if input_password != "696238":
+# 			input_password = input("틀렸습니다. 다시 입력하세요 : ")
 
-	if input_password != "696238":
-		sys.exit()
+# 	if input_password != "696238":
+# 		sys.exit()
 
 
 
 # 오늘에 대한 정보 입력. 이후 각각 파일에 저장될 것임.
 today = datetime.date.today()
-def which_day(date):
+def which_day(date): 						# 입력한 날짜가 무슨 요일인지 반환하는 함수.
 	return ['월', '화','수','목','금','토','일'][date.weekday()]
+
+
 
 
 
@@ -54,8 +58,9 @@ while True:
 	menu_selected = input("원하시는 기능을 입력해주세요. : ")
 
 	while menu_selected not in ["1","2","3","9"]:
-		input("4가지의 숫자 중 원하시는 것을 정확히 입력해주세요. : ")
+		menu_selected = input("4가지의 숫자 중 원하시는 것을 정확히 입력해주세요. : ")
 
+	#프로그램 종료 사인.
 	if menu_selected == "9":
 		print("감사합니다. 프로그램을 종료합니다.")
 		for i in range(3):
@@ -63,12 +68,43 @@ while True:
 			time.sleep(1)
 		break	
 
+# 파일 업로드 :
+	total_record = []
+	with open('daily_expenditure.txt', 'rb') as money:
+		while True:
+			try:
+				total_record.append(pickle.load(money))
+			except:
+				break
+	if total_record == []:
+		recent_record = [];
+	else:
+		recent_record = total_record[-1]
 
 
 #####################################
 #### menu 1. 지출금액 추가 기능.
 #####################################
-#### 만약 오늘 처음 금액 지출을 입력한다면 입력되고, 두 번째 이상일 경우 기존 입력된 금액에 추가한다.
+	# 기록되는 자료 형식은 다음과 같다. 기록되는 텍스트는 pickle package를 활용하도록 한다.
+	""" data_format =  {'today':str(today), 'weekday':which_day(today), 'year':today.year, 'month':today.month, 'day':today.day, 'money_used':10000}"""
+	
 
+#### 1.1 - 만약 오늘 처음 금액 지출을 입력한다면 입력되고, 1.2 - 두 번째 이상일 경우 기존 입력된 금액에 추가한다.
+	if menu_selected == "1":
+		# 1.1 
+		if recent_record == [] or recent_record['today'] != str(today):
+			print()
+			print("오늘의 처음 입력이십니다.")
+			money_spent = input("오늘 사용하신 금액을 입력해주세요. : ")
+			while not money_spent.isnumeric():
+				input("숫자를 입력하셔야 합니다. 다시 입력하세요 : ")
+			money_spent = int(money_spent)
 
-# 전체 날짜에 대한 지출을 받는다. 변수명은 daily_spent_all 로 하겠다.
+			record = {'today':str(today), 'weekday':which_day(today), 'year':today.year, 'month':today.month, 'day':today.day, 'money_used':money_spent}
+			with open('daily_expenditure.txt', 'ab') as money:
+				pickle.dump(record, money)
+
+			print('\n')
+			print("현재 시각",datetime.datetime.now()," 오늘 하루 사용하신 금액은", record[money_spent])
+
+		
